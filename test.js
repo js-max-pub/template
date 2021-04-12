9aab9399381a25212eef351f23a3a440e844afd9
Max Fechner <max.fechner@gmail.com>
2021-04-12 11:36:37 +0200

    - renaming

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import { FS } from 'https://js.max.pub/fs/deno.js'
-import template from './src.js';
+import template from './mod.js';
 
 let test = FS.folder('test', import.meta);
 
@@ -7,11 +7,11 @@ let person = test.file('person.json').json
 console.log(person)
 
 
-let test1 = template(test.file('test1.isi.html').text, { debug: true })
-test.file('test1.isi.js').text = test1.toString()
-test.file('test1.result.html').text = test1(person)
+let test1 = template(test.file('test1.isi.html',import.meta).text, { debug: true })
+test.file('test1.isi.js',import.meta).text = test1.toString()
+test.file('test1.result.html',import.meta).text = test1(person)
 
 
-let test2 = template(test.file('test2.isi.md').text)
-test.file('test2.isi.js').text = test2.toString()
-test.file('test2.result.md').text = test2({ person })
+let test2 = template(test.file('test2.isi.md',import.meta).text)
+test.file('test2.isi.js',import.meta).text = test2.toString()
+test.file('test2.result.md',import.meta).text = test2({ person })
